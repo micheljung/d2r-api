8f4993aeb2d60225bb40b74c68c11029f8dc2e64
Michel Jung <michel.jung89@gmail.com>
2021-10-10 15:54:47 +0200

    Make DTO generator rely on the output of data-extractor

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -48,8 +48,13 @@ sourceSets["main"].java {
   srcDir("build/generated/sources/d2r")
 }
 
-d2rDtoGenerator {
+d2rExtractor {
   source.set(File("C:/Program Files (x86)/Diablo II Resurrected/Data"))
+  target.set(layout.projectDirectory.dir("src/main/resources/d2r"))
+}
+
+d2rDtoGenerator {
+  source.set(d2rExtractor.target)
   target.set(layout.buildDirectory.dir("generated/sources/d2r"))
 }
 
--- build-plugins/dto-generator/src/main/kotlin/DtoGeneratorPlugin.kt
+++ build-plugins/dto-generator/src/main/kotlin/DtoGeneratorPlugin.kt
@@ -1,12 +1,11 @@
 package ch.micheljung.d2rapi.dto
 
-import ch.micheljung.d2rapi.extract.DataExtractorPluginExtension
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.file.DirectoryProperty
 
 interface DtoGeneratorPluginExtension {
-  /** The Diablo 2: Resurrected Data directory. */
+  /** The directory containing the directory structure to the extracted excel files. */
   val source: DirectoryProperty
 
   /** Where to write the DTOs. */
@@ -18,18 +17,8 @@ class DtoGeneratorPlugin : Plugin<Project> {
     val extension = target.extensions.create("d2rDtoGenerator", DtoGeneratorPluginExtension::class.java)
 
     target.tasks.register("generate", GeneratorTask::class.java) { task ->
-      val dataExtractFolder = target.buildDir.resolve("resources/main")
-
-      task.dataFolder.set(dataExtractFolder)
+      task.dataFolder.set(extension.source)
       task.dtoFolder.set(extension.target)
-
-      target.plugins.withId("ch.micheljung.d2rapi.data-extractor") {
-        target.extensions.configure<DataExtractorPluginExtension>("d2rExtractor") {
-          it.source.set(extension.source.get())
-          it.target.set(dataExtractFolder)
-        }
-      }
-
       task.dependsOn("extract")
     }
 
