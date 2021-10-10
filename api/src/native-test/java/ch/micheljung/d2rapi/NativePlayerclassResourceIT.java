07673ff47491fa89f24a02a1337d66250e62ed85
Michel Jung <michel.jung89@gmail.com>
2021-10-10 15:59:15 +0200

    Remove native (unimplemented) integration test

--- api/src/native-test/java/ch/micheljung/d2rapi/NativePlayerclassResourceIT.java
+++
@@ -1,9 +0,0 @@
-package ch.micheljung.d2rapi;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativePlayerclassResourceIT extends PlayerclassResourceTest {
-
-    // Execute the same tests but in native mode.
-}
