eae7674002c2be492ec2109c14e7b6e2b98c224b
Michel Jung <michel.jung89@gmail.com>
2021-10-17 13:09:00 +0200

    Clean up rollup.config.js

--- api/rollup.config.js
+++ api/rollup.config.js
@@ -1,7 +1,3 @@
-import {nodeResolve} from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import json from '@rollup/plugin-json';
-
 export default {
   input: 'index.mjs',
   output: {
